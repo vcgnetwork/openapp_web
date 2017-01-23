0774db5dcfe9bf5825245c1b009844c1681d4ad5
Web Developer <webdev@mini.local>
2017-01-23 12:43:15 -0500

    updated cities table and seeder

--- database/migrations/2017_01_23_023836_create_cities_table.php
+++ database/migrations/2017_01_23_023836_create_cities_table.php
@@ -16,9 +16,19 @@ class CreateCitiesTable extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->string('zipcode');
+            $table->string('country');
+            $table->string('lat');
+            $table->string('long');
+            $table->string('wuiurl');
             $table->string('city');
             $table->string('state');
-            $table->string('zipcode');
+            $table->string('observation_time');
+            $table->string('weather');
+            $table->string('temperature_string');
+            $table->string('wind_string');
+            $table->string('icon_url');
+            $table->string('forecast');
             $table->unique(['user_id', 'zipcode']);
             $table->timestamps();
             $table->softDeletes();
